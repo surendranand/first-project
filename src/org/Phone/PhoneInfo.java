8dff149f0feb5e6bf22f8376c9743e40944ada66
surendranand <surendranrules@gmail.com>
2023-06-06 10:56:15 +0530

    phone

---
+++ src/org/Phone/PhoneInfo.java
@@ -0,0 +1,29 @@
+package org.Phone;
+
+public class PhoneInfo {
+	private void phoneName() {
+		System.out.println("phonename: "+ "oppo");
+	}
+	private void phoneMieiNum() {
+		System.out.println("mieinumber: "+"123456");
+	}
+	private void camera() {
+		System.out.println("camera: "+ "12mpixel");
+	}
+	private void storage() {
+		System.out.println("storage: "+ "512gb");
+	}
+	private void osName() {
+		System.out.println("OSname: "+"andriod");
+	}
+public static void main(String[] args) {
+	PhoneInfo gg= new PhoneInfo();
+	gg.phoneName();
+	gg.phoneMieiNum();
+	gg.camera();
+	gg.storage();
+	gg.osName();
+	
+}
+
+}
